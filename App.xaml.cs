f5035fcf51590725d3f988e08beacb6117a58a87
fran456ys <francoysrraelit@gmail.com>
2025-08-11 19:47:41 -0300

    primer commit

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace GestorDePlanillas
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
