297bef5c31ef4eeda586c5853a34b9348d7a72ed
fran456ys <francoysrraelit@gmail.com>
2025-08-12 04:01:16 -0300

    added models and modern styles

---
+++ Model/PlanillaRenderer.cs
@@ -0,0 +1,70 @@
+﻿using System.Globalization;
+using System.IO;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace GestorDePlanillas.Model
+{
+    internal class PlanillaRenderer
+    {
+        public BitmapSource GenerarPlanilla(Ternero t, string rutaPlantilla)
+        {
+            // Cargar plantilla
+            var plantilla = new BitmapImage(new Uri(rutaPlantilla, UriKind.RelativeOrAbsolute));
+
+            // Crear superficie de dibujo
+            var visual = new DrawingVisual();
+
+            // Obtener DPI (pixelsPerDip)
+            var dpi = VisualTreeHelper.GetDpi(visual).PixelsPerDip;
+
+            using (var dc = visual.RenderOpen())
+            {
+                // Dibujar imagen base
+                dc.DrawImage(plantilla, new Rect(0, 0, plantilla.PixelWidth, plantilla.PixelHeight));
+
+                // Configuración de texto
+                var tipo = new Typeface("Arial");
+                var brush = Brushes.Black;
+
+                // Ejemplo: Nombre
+                dc.DrawText(
+                    new FormattedText(
+                        t.nombre,
+                        CultureInfo.InvariantCulture,
+                        FlowDirection.LeftToRight,
+                        tipo,
+                        20,
+                        brush,
+                        dpi // PixelsPerDip
+                    ),
+                    new Point(100, 150) // posición
+                );
+
+                
+            }
+
+            // Renderizar a BitmapSource
+            var bmp = new RenderTargetBitmap(
+                plantilla.PixelWidth,
+                plantilla.PixelHeight,
+                96, 96,
+                PixelFormats.Pbgra32
+            );
+            bmp.Render(visual);
+
+            return bmp;
+        }
+
+        public void GuardarComoPng(BitmapSource bmp, string rutaSalida)
+        {
+            var encoder = new PngBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(bmp));
+            using (var fs = new FileStream(rutaSalida, FileMode.Create))
+            {
+                encoder.Save(fs);
+            }
+        }
+    }
+}
---
+++ Model/Ternero.cs
@@ -0,0 +1,25 @@
+﻿namespace GestorDePlanillas.Model
+{
+    internal class Ternero
+    {
+        public string nombre { get; set; }
+        public string rp { get; set; }
+        public string rDeControl { get; set; }
+        public string raza { get; set; }
+        public string hba { get; set; }
+        public string chapa { get; set; }
+        public string color { get; set; }
+        public string fechaNac { get; set; } //hay que ver a la hora de importar el csv
+        public string tambo { get; set; }
+        public string padreRP { get; set; }
+        public string padreHBA { get; set; }
+        public string nombrePadre { get; set; }
+        public string madreRP { get; set; }
+        public string madreHBA { get; set; }
+        public string madreCAL { get; set; }
+        public string nombreMadre { get; set; }
+
+        public string fotoIzquierda { get; set; }
+        public string fotoDerecha { get; set; }
+    }
+}
