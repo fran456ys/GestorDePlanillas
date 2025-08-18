73432528644edc97b23ae2fa368a3d9b1b9fcbf8
fran456ys <francoysrraelit@gmail.com>
2025-08-18 13:21:51 -0300

    added ContentControls

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using GestorDePlanillas.Model;
+using GestorDePlanillas.Views.UserControls;
 using System.Collections.ObjectModel;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace GestorDePlanillas
 {
@@ -12,10 +14,10 @@ namespace GestorDePlanillas
         public MainWindow()
         {
             InitializeComponent();
-            dgTerneros.ItemsSource = terneros;
-            tbImportador.ArchivoSeleccionado += TbImportador_ArchivoSeleccionado;   
+            //dgTerneros.ItemsSource = terneros;
+            //tbImportador.ArchivoSeleccionado += TbImportador_ArchivoSeleccionado;   
         }
-
+        /*
         private void TbImportador_ArchivoSeleccionado(object sender, string filePath)
         {
             try
@@ -27,6 +29,50 @@ namespace GestorDePlanillas
             {
                 MessageBox.Show($"Error al importar archivo: {ex.Message}");
             }
+        }*/
+
+        private void dgTerneros_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
+        {
+            if (e.PropertyName == "fotoIzquierda" || e.PropertyName == "fotoDerecha")
+            {
+                e.Cancel = true; // oculta esas columnas
+            }
+        }
+
+        private void StackPanelDashBoard_ImportarExcelClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void StackPanelDashBoard_CargarTerneroClick(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new CargarTerneroView();
+        }
+
+        private void StackPanelDashBoard_ReportesClick(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
+
+/*
+ * <Grid Grid.Column="2" Margin="10">
+            <Grid.RowDefinitions>
+                <RowDefinition Height="100"/>
+                <RowDefinition Height="*"/>
+            </Grid.RowDefinitions>
+
+            <UserControls:PlaceHolderTextBox Width="800" x:Name="tbImportador" HorizontalAlignment="Left"/>
+
+            <DataGrid x:Name="dgTerneros" Grid.Row="1" Width="1300" HorizontalAlignment="Left"
+                      AutoGenerateColumns="True" IsReadOnly="True" Background="White" AutoGeneratingColumn="dgTerneros_AutoGeneratingColumn"
+                      HorizontalGridLinesBrush="#B8CFCE" />
+
+            <ui:SimpleStackPanel Grid.Row="1" HorizontalAlignment="Right" Width="300" Margin="35">
+                <Button Style="{StaticResource StackButton}" Content="Visualizar Planilla"/>
+                <Button Style="{StaticResource StackButton}" Content="Descargar Planillas"/>
+                <Button Style="{StaticResource StackButton}" Content="Imprimir Planillas"/>
+            </ui:SimpleStackPanel>
+        </Grid>
+*/
--- Views/UserControls/StackPanelDashBoard.xaml.cs
+++ Views/UserControls/StackPanelDashBoard.xaml.cs
@@ -1,28 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace GestorDePlanillas.Views.UserControls
 {
-    /// <summary>
-    /// Lógica de interacción para StackPanelDashBoard.xaml
-    /// </summary>
     public partial class StackPanelDashBoard : UserControl
     {
+        public event RoutedEventHandler ImportarExcelClick;
+        public event RoutedEventHandler CargarTerneroClick;
+        public event RoutedEventHandler ReportesClick;
+
         public StackPanelDashBoard()
         {
             InitializeComponent();
+        }
+
+        private void btnCargarTernero_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            CargarTerneroClick?.Invoke(this, e);
+        }
+
+        private void btnImportarExcel_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            ImportarExcelClick?.Invoke(this, e);
+        }
+
+        private void btnReportes_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            ReportesClick?.Invoke(this, e);
         }
     }
 }
